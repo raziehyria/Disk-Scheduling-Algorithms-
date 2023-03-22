0b3fd8d6a1a607d5694484cadb65173f6c7eea71
raziehyria <71193841+raziehyria@users.noreply.github.com>
2023-03-22 01:03:11 -0400

    updated cscan for more accuracy

--- RHyria_DSA_CMPSC472.py
+++ RHyria_DSA_CMPSC472.py
@@ -21,9 +21,9 @@ REQUESTS = 1000 # constant value representing requests
 # FCFS scheduling
 def fcfs(initial_position, requests):
     total_head_movement = 0
-    current = initial_position # Read the initial position of the disk head.
+    current = initial_position # read the initial position of the disk head.
     for r in requests:
-        total_head_movement += abs(r - current) # Calculate the distance between the initial head position and the first request.
+        total_head_movement += abs(r - current) # calculate the distance between the initial head position and the first request.
         current = r
     return total_head_movement # return the total distance traveled.
 
@@ -32,8 +32,8 @@ def fcfs(initial_position, requests):
 def scan(initial_position, requests):
     total_head_movement = 0
     current = initial_position
-    queue = sorted(r for r in requests if r >= initial_position) # Sort the sequence of requests in ascending order.
-    for r in queue: #Find the index of the request that is closest to the initial head position
+    queue = sorted(r for r in requests if r >= initial_position) # sort the sequence of requests in ascending order.
+    for r in queue: # find the index of the request that is closest to the initial head position
         total_head_movement += abs(current - r) # service those requests.
         current = r
     queue = sorted(r for r in requests if r < initial_position)
@@ -44,25 +44,38 @@ def scan(initial_position, requests):
 
 
 # C-SCAN scheduling
-def cscan(initial_position, requests):
-    total_head_movement = 0
-    current = initial_position
-    queue = sorted(requests + [initial_position]) # Sort the sequence of requests in ascending order.
-    left = [r for r in queue if r < initial_position] # Find the index of the request that is closest to the initial head position.
-    right = [r for r in queue if r >= initial_position] # Find the index of the request that is opposite to the initial head position.
-    for r in right:
-        total_head_movement += abs(current - r) #Service all requests from the initial 
-        current = r
-    current = 0 # reset position of the head 
-    for r in left + [max(queue)]:
-        total_head_movement += abs(current - r) #Service all requests from the opposite side 
-        current = r
-    return total_head_movement# return the total distance traveled.
+def c_scan(initial_position, requests):
+    queue = sorted(requests) # sort the sequence of requests in ascending order.
+    direction = 1
+    current = initial_position # intialize starting value and direction
+    total_head_movement = 0 #movement counter
+    
+    while queue: # while requests in the queue
+        next_req = None
+        for req in queue:
+            if direction == 1 and req >= current: # looking for request in pos direction
+                next_req = req
+                break
+            elif direction == -1 and req <= current:# looking for request in neg direction
+                next_req = req
+                break
+        
+        if next_req is None: # no requests in the current direction, switch direction
+            direction = -direction
+            continue
+        
+        distance = abs(next_req - current) # calculate distance between current head and request
+        total_head_movement += distance # add distance to total head movement, 
+        current = next_req # update the head position to the request,
+        queue.remove(next_req) # and remove the request from the queue
+    return total_head_movement
+
 
 # main method to call on the functions above
 if __name__ == '__main__':
     if len(sys.argv) != 2: # checking to see if user inputed proper parameters
-        print("Usage: python RHyria_DSA_CMPSC472.py.py initial_position")
+        print("Usage: python RHyria_DSA_CMPSC472.py.py <#initial_position>")
+        print("\nHINT: Try numbers like 0, 100, 500, or random.")
         sys.exit(1) 
     
     initial_position = int(sys.argv[1]) # initializing the head based off user input
@@ -71,6 +84,6 @@ if __name__ == '__main__':
     requests = [random.randint(0, CYLINDERS-1) for _ in range(REQUESTS)]
    
     # Run the algorithms and print the results
-    print("FCFS scheduling:", fcfs(initial_position, requests))
-    print("SCAN scheduling:", scan(initial_position, requests))
-    print("C-SCAN scheduling:", cscan(initial_position, requests))
+    print("FCFS total head movement:", fcfs(initial_position, requests))
+    print("SCAN total head movement:", scan(initial_position, requests))
+    print("C-SCAN total head movement:", c_scan(initial_position, requests))
